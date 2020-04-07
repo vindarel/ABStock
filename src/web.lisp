4388f1224efd78c90135d3913bed8ecef817ecf7
vindarel <vindarel@mailz.org>
2020-04-07 17:57:01 +0200

    basket: icon with qty, calc. total, remove elt.

--- src/web.lisp
+++ src/web.lisp
@@ -8,6 +8,7 @@
 (defparameter +base.html+ (djula:compile-template* "base.html"))
 (defparameter +welcome.html+ (djula:compile-template* "welcome.html"))
 (defparameter +cards.html+ (djula:compile-template* "cards.html"))
+(defparameter +panier.html+ (djula:compile-template* "panier.html"))
 
 ;; Custom Djula filter to format prices.
 ;; There is also the "format" filter.
@@ -29,8 +30,35 @@
                           :title "La Palpitante en ligne"))
 
 (easy-routes:defroute search-route ("/search" :method :get) (q)
+  (format t "~& /search ~&")
   (djula:render-template* +cards.html+ nil
+                          :title (format nil "La Palpitante - ~a" q)
                           :cards (subseq *cards* 0 10)))
 
+(easy-routes:defroute panier-route ("/panier" :method :get) (ids)
+  (format t "~& /panier ~&")
+  (let* ((ids-list (str:split "," ids :omit-nulls t))
+         (ids-list (mapcar (lambda (it)
+                             (parse-integer it))
+                           ids-list))
+         (allcards (subseq *cards* 0 10)) ;; DEV
+         (cards (loop for id in (print ids-list)
+                   for position = (position id allcards :key (lambda (card)
+                                                               (getf card :|id|)))
+                   when position
+                   ;; do (setf (getf (elt *cards* position) :|basketqty|)
+                   ;;          (count id ids-list))
+                   collect  (elt *cards* position)))
+         ;; (cards (remove-duplicates cards
+         ;;                           :key (lambda (card)
+         ;;                                  (getf card :|id|))))
+         )
+    ;; (format t "-- cards: ~a" cards)
+    (djula:render-template* +panier.html+ nil
+                          :title (format nil "La Palpitante - Mon Panier")
+                          :cards cards)))
+
 (defun start ()
+  (unless *connection*
+    (setf *connection* (connect)))
   (hunchentoot:start *server*))
