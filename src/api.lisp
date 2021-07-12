8c3f741a449603d9332407b0e2d999743cacf03b
vindarel <vindarel@mailz.org>
2021-07-12 14:12:20 +0200

    add search API /api/v1/search.json?query=foo, 5min cache
    requires a non-blank query.

--- src/api.lisp
+++ src/api.lisp
@@ -11,3 +11,14 @@
 (easy-routes:defroute api-newly-created-route ("/api/v1/lastcreated.json" :method :get) ()
   (setf (hunchentoot:content-type*) "application/json")
   (jojo:to-json (last-created-cards)))
+
+(defcached (search-cards-with-cache :timeout (* 60 5)) (query)
+  "Search cards in stock with keywords.
+  Results are cached for 5min."
+  (search-cards query))
+
+(easy-routes:defroute api-search-route ("/api/v1/search.json" :method :get) (query)
+  (setf (hunchentoot:content-type*) "application/json")
+  (jojo:to-json (if (str:non-blank-string-p query)
+                    (search-cards-with-cache query)
+                    (values))))
