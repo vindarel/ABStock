1c1e1dd9b51b0c51a164a84ac04a99fe29e63636
vindarel <vindarel@mailz.org>
2021-07-12 14:37:21 +0200

    search API: if ?all=t, return all cards in stock
    works flawlessly with 2500 cards. Supposed to be used with ±300.

--- src/api.lisp
+++ src/api.lisp
@@ -17,8 +17,14 @@
   Results are cached for 5min."
   (search-cards query))
 
-(easy-routes:defroute api-search-route ("/api/v1/search.json" :method :get) (query)
+(easy-routes:defroute api-search-route ("/api/v1/search.json" :method :get) (query all)
+  "Search with keywords.
+If `all' is non-blank, return all cards in stock."
   (setf (hunchentoot:content-type*) "application/json")
-  (jojo:to-json (if (str:non-blank-string-p query)
-                    (search-cards-with-cache query)
-                    (values))))
+  (cond
+    ((str:non-blank-string-p all)
+     (jojo:to-json (search-cards-with-cache "")))
+    (t
+     (jojo:to-json (if (str:non-blank-string-p query)
+                       (search-cards-with-cache query)
+                       (values))))))
